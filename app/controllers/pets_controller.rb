41f75d59330793b99af203a1da2f21ec26dc3607
Bryce B <bryce.boddy@gmail.com>
2020-08-04 08:56:09 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,12 @@ class PetsController < ApplicationController
   post '/pets' do 
     @pet = Pet.create(params[:pet])
     
+    if !params["owner"]["name"].empty?
+      #/redirect '/failure'
+    else
+      
+    end
+    
     redirect to "pets/#{@pet.id}"
   end
 
